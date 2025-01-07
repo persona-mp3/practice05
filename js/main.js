13da5365f1e545f902d7d9dd26481e0f9856ac0f
nninfatuation <aiwuyoeghosa@outlook.com>
2025-01-07 00:11:57 +0000

    new pages

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
+const navLinks = document.querySelectorAll('.link');
+navLinks.forEach((link) => {
+    link.addEventListener('click', ()=>{
+        navLinks.forEach(link => link.classList.remove('active'));
+        link.classList.add('active')
+    });
+
+    // link.addEventListener('')
+});
+
 document.addEventListener('DOMContentLoaded', () => {
     gsap.registerPlugin(ScrollTrigger);
 
 })
 
-// gsap.from('.image-container', {duration: 3.5, x: '400%', ease: 'expo'});
 gsap.from('.image-container', {
     scrollTrigger : {
         trigger: '.image-container',
@@ -16,28 +25,3 @@ gsap.from('.image-container', {
     ease: 'expo'
 });
 
-let elements = document.querySelectorAll('.text');
-elements.forEach((element) => {
-    //get inner text
-    const innerText = element.innerText
-    element.innerHTML = ''; //?
-
-    let textContainer = document.createElement('div');  //create div element for each word
-    textContainer.classList.add('block');       //add block class to each div for each letter
-
-
-    for (let character of innerText) {
-        let span = document.createElement('span');
-        span.innerText = character.trim() === ""?"/xa0" : character;
-        span.classList.add('letter');
-
-        textContainer.appendChild(span);
-
-    }
-
-    element.appendChild(textContainer);
-    element.appendChild(textContainer.cloneNode(true));
-});
-
-elements.addEventListener('mouseover', () => {
-    element.classList.remove('play')})
