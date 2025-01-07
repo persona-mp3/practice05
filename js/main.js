d532e9bea01a0a8076d0692b4ef53dc1b01d3c0a
nninfatuation <aiwuyoeghosa@outlook.com>
2025-01-07 00:11:57 +0000

    new pages

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
+const navLinks = document.querySelectorAll('.link');
+const redirect = document.querySelector('.caption');
+
+navLinks.forEach((link) => {
+    link.addEventListener('click', ()=>{
+        navLinks.forEach(link => link.classList.remove('active'));
+        link.classList.add('active')
+    });
+});
+
+redirect.addEventListener('click', () => {
+    window.location.href = 'index.html'
+})
+
 document.addEventListener('DOMContentLoaded', () => {
     gsap.registerPlugin(ScrollTrigger);
 
 })
 
-// gsap.from('.image-container', {duration: 3.5, x: '400%', ease: 'expo'});
 gsap.from('.image-container', {
     scrollTrigger : {
         trigger: '.image-container',
         toggleActions: 'play none none none',
-        // scrub: 1,
     },
 
     duration: 2.5,
@@ -16,28 +28,3 @@ gsap.from('.image-container', {
     ease: 'expo'
 });
 
-let elements = document.querySelectorAll('.text');
-elements.forEach((element) => {
-    //get inner text
-    const innerText = element.innerText
-    element.innerHTML = ''; //?
-
-    let textContainer = document.createElement('div');  //create div element for each word
-    textContainer.classList.add('block');       //add block class to each div for each letter
-
-
-    for (let character of innerText) {
-        let span = document.createElement('span');
-        span.innerText = character.trim() === ""?"/xa0" : character;
-        span.classList.add('letter');
-
-        textContainer.appendChild(span);
-
-    }
-
-    element.appendChild(textContainer);
-    element.appendChild(textContainer.cloneNode(true));
-});
-
-elements.addEventListener('mouseover', () => {
-    element.classList.remove('play')})
